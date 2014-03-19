fc9cf9e081eef5b2b70bbe103ef7f915dc2b9768
andrewbuie <Andrew@wl-10-190-77-146.wireless.duke.edu>
2014-03-19 18:52:11 -0400

    attached servos and added forward back left right methods

--- src/BaseLineFollowing/BaseLineFollowing.ino
+++ src/BaseLineFollowing/BaseLineFollowing.ino
@@ -8,10 +8,39 @@ From the POV of the Bot
 #define IRRC 5
 #define IRLC 6
 #define IRL 7
+#include <Servo.h>
 
-void setup() {
-  
+Servo leftServo; //define servos
+Servo rightServo;
+
+void setup() 
+{
+  leftServo.attach(13); //attach servos
+  rightServo.attach(12);
+  leftServo.write(90); //set to no movement
+  rightServo.write(90);
 }
+
+void Forward(){
+rightServo.write(0); //right full forward
+leftServo.write(180); //left full forward
+} 
+
+void Back(){
+rightServo.write(180); //right full backwards
+leftServo.write(0); //left full backwards
+}
+
+void Left(){
+rightServo.write(90);
+leftServo.write(180);
+}
+
+void Right(){
+rightServo.write(0);
+leftServo.write(90);
+}
+  
 long RCtime(int sensPin){
    long result = 0;
    pinMode(sensPin, OUTPUT);       // make pin OUTPUT
@@ -25,8 +54,16 @@ long RCtime(int sensPin){
    }
 
    return result;                   // report results   
-}   
+} 
 
 void loop() {
   Serial.println(RCtime(IRR));
+  Forward();
+  delay(1000);
+  Back();
+  delay(1000);
+  Left();
+  delay(1000);
+  Right();
+  delay(1000);
 }
