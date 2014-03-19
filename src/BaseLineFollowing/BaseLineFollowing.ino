957e25de4cc98a88f8e21d7af06d483b4551846a
Austin McKee <andale@wl-10-190-79-9.wireless.duke.edu>
2014-03-19 18:37:48 -0400

    RCtime function added

--- src/BaseLineFollowing/BaseLineFollowing.ino
+++ src/BaseLineFollowing/BaseLineFollowing.ino
@@ -3,10 +3,30 @@ Line Following Base Code with Four Sensors
 Digital Inputs should be 4-7, Right to Left
 From the POV of the Bot
 */
-void setup() {
 
+#define IRR 4
+#define IRRC 5
+#define IRLC 6
+#define IRL 7
+
+void setup() {
+  
 }
+long RCtime(int sensPin){
+   long result = 0;
+   pinMode(sensPin, OUTPUT);       // make pin OUTPUT
+   digitalWrite(sensPin, HIGH);    // make pin HIGH to discharge capacitor - study the schematic
+   delay(1);                       // wait a  ms to make sure cap is discharged
+
+   pinMode(sensPin, INPUT);        // turn pin into an input and time till pin goes low
+   digitalWrite(sensPin, LOW);     // turn pullups off - or it won't work
+   while(digitalRead(sensPin)){    // wait for pin to go low
+      result++;
+   }
+
+   return result;                   // report results   
+}   
 
 void loop() {
-
+  Serial.println(RCtime(IRR));
 }
