f24ceecab856f8109c0c5739395c79cca6f1e5db
andrewbuie <buie.andrew@gmail.com>
2014-04-15 22:23:53 -0400

    new communication

--- src/TeamYellow/TeamYellow5BotComm/TeamYellow5BotComm.ino
+++ src/TeamYellow/TeamYellow5BotComm/TeamYellow5BotComm.ino
@@ -55,9 +55,9 @@ void setup()
 {
   Serial.begin(9600);                                     //start computer serial
   Xbee.begin(9600);                                       //start xbee
-  sDelay(500);                                            //wait half a second and run xbee
+  delay(500);                                            //wait half a second and run xbee
   Move(0,0);                                              //stop
-  sDelay(500);                                            //wait half a second and run xbee
+  delay(500);                                            //wait half a second and run xbee
   
   
   leftServo.attach(13);                                   //attach left servo
@@ -74,7 +74,7 @@ void setup()
   digitalWrite(ledyellow, LOW);
   digitalWrite(ledgreen, LOW);
   
-  sDelay(100);                                            //wait 1/10 second and run xbee
+  delay(100);                                            //wait 1/10 second and run xbee
 }
 
 void loop() 
@@ -104,46 +104,49 @@ void loop()
   Move(0,0);                                              //stops moving
   leftServo.detach();                                     //detach left servo
   rightServo.detach();                                    //detach right servo
-  sDelay(500);                                            //wait while receiving xbee communication for half a second
-  foundOrder(positionNum);   
+  delay(500);                                            //wait while receiving xbee communication for half a second
+  //foundOrder(positionNum);   
    
 
    
-  positionNum = doIGo();
-  debug("DoIGo finished",-100);
-  sDelay(3200);
-  communicating = false;
+  //positionNum = doIGo();
+  //debug("DoIGo finished",-100);
+  waitForSignal(positionNum);
+  delay(3200);
+  //communicating = false;
    
   leftServo.attach(13);                                   //attach left servo
   rightServo.attach(12);                                  //attach right servo
-  sDelay(200);                                            //wait and read xbee
-  debug("servos attached",-100);
+  delay(200);                                            //wait and read xbee
+  //debug("servos attached",-100);
   
   lineFollow(4, -1, -1L);                                 //line follow until sharp final turn
-  debug("finished white follow",-100);
+  //debug("finished white follow",-100);
    
   Move(.1, 3);                                            //manually correct for hard final turn
-  sDelay(500);                                            //wait half a second
+  delay(500);                                            //wait half a second
   Move(1,1);                                              //go straight
-  sDelay(400);                                            //wait
+  delay(400);                                            //wait
 
-  debug("finished correction movement",-100);
-  communicating = true; 
-  sendMoving();                                           //tell next bot to go
+  //debug("finished correction movement",-100);
+  //communicating = true; 
+  //sendMoving();                                         //tell next bot to go
+  
   startTime = millis();                                   //start timer
   lineFollow(-1,-1,1200L);                                //blindly line follow for 1.2 seconds
 
    
-  debug("sending moving",-100);
-  debug("beginning final",-100);
-  lineFollow(-1, 6-positionNum,-1L);                      //get to final hash
+  //debug("sending moving",-100);
+  //debug("beginning final",-100);
+  lineFollow(-1, ((6-positionNum)+3),-1L);                      //get to final hash
+  imGoing(positionNum);
 
   Move(0,0);                                              //stop
   leftServo.detach();                                     //detach left servo           
   rightServo.detach();                                    //detach right servo
   while(1)                                                //stop indefinitely
   {
-    sDelay(1000);
+    delay(1000);
     Move(0,0);
   }
 }
@@ -259,7 +262,7 @@ long RCtime(int sensPin)
    long result = 0;
    pinMode(sensPin, OUTPUT);       // make pin OUTPUT
    digitalWrite(sensPin, HIGH);    // make pin HIGH to discharge capacitor - study the schematic
-   sDelay(1);                       // wait a  ms to make sure cap is discharged
+   delay(1);                       // wait a  ms to make sure cap is discharged
 
    pinMode(sensPin, INPUT);        // turn pin into an input and time till pin goes low
    digitalWrite(sensPin, LOW);     // turn pullups off - or it won't work
@@ -284,7 +287,7 @@ void onWhite()
     if ((millis() - whiteLastTime) > whiteOverrideTime) 
     {
       whiteCount++;
-      debug("White added",-100);
+      //debug("White added",-100);
       whiteLastTime = millis();
     }
   }
@@ -298,7 +301,7 @@ void onBlack()
     if ((millis() - blackLastTime) > blackOverrideTime) 
     {
       blackCount++;
-      debug("Black Added",-100);
+      //debug("Black Added",-100);
       blackLastTime = millis();
     }
   }
@@ -306,366 +309,53 @@ void onBlack()
 
 
 
-
-
-
-
-//Below is Communication
-
-// Delay function that allows for constant communication
-// Can also be used with mills = 0 for calling as often as you want
-void sDelay(int mills) { 
-  if (millis() - communicateTimer > 20L) {
-   if (communicating) {
-     communicate();
-   }
-     communicateTimer = 0L;
-  } 
-  for (int j=0;j < mills; j+=5) {
-    delay(5);
-    if (millis() - communicateTimer > 20L) {
-     if (communicating) {
-       communicate();
+void imGoing(int pos) 
+{
+   char keymap[] =  "yuiop";
+   Xbee.print(keymap[pos]);
+}
+ 
+//This function will wait until your bot recieves a message to go. (The bot in front of you in line should 
+//send this mesage.) There is a timeout that corresponds to your bot number. There is a grand timeout of 45s.
+//If you send -1, your bot will go at 45 seconds.
+void waitForSignal(int pos) 
+{
+   int now = millis();
+   while(1) {
+     //if i'm bot 1, I go
+     if (pos == 1) {
+       //time to go!
+       return; 
      }
-      communicateTimer = 0L;
-    }
-  }
-}
-
-// Create a one character hexidecimal checksum (ascii a-f,0-9)
-String checksum(String data) 
-{
-  int sum = 0;
-  double math = 0;
-  
-  for (int i=0L;i<data.length();i++) 
-  {
-    sum += (int) ((char) data.charAt(i));
-  }
-  math = (double) sum;
-  // The complicated mathematical hash generator. 1.8 is a good number.
-  math = pow(math,2) / 10.0;
-  sum = ((int) math) % 16;
-  
-  if (sum < 10) 
-  {
-    // Return String character 0-9 (ascii 48-57)
-    return String(char(sum + 48));
-  } 
-  else 
-  {
-    // Return String character a-f (ascii 97-102)
-    return String(char(sum + 87));
-  }
-}
-
-// Overarching Send/Recieve Function
-void communicate() 
-{
-  String buffer = "";
-  String filtered = "";
-  int len;
-  int i;
-  int readChar = 1;
-  
-  // Read whole buffer into String
-  while (Xbee.available() && readChar <=3) 
-  {
-    //Read Character
-    char receiving = Xbee.read();
-    buffer += String(receiving);
-    readChar++;
-  }
-  if (buffer != "") 
-  {
-    //debug("Buffer: "+buffer,-100);
+     
+     //the values
+     char keymap[] =  "yuiop";
+     char theirKey = keymap[pos-1];
     
-    // Take away all non wanted characters. Keep a-k,G-K, 1-9, and '='
-    for (i=0;i<buffer.length();i++) 
-    {
-      char c = buffer.charAt(i);
-      if ((c>=97 && c<=107) || (c>=71 && c<=75) || (c>=48 && c<=57) || (c == 61)) 
-      {
-        filtered += String(c);
+     //read xbee until the person in front of you says going
+     while (Xbee.available()) {
+      //Read Character
+      char receiving = Xbee.read();
+      //if char is the id of the bot in front of me, I should go
+      if (receiving == theirKey) {
+       //my turn
+       return; 
       }
     }
     
-    // If we have the start of a 'packet'
-    if (String(filtered.charAt(0)) == "=") 
-    {
-      String data = String(filtered.charAt(1));
-      String hash = String(filtered.charAt(2));
-      
-      // Check for integrity of packet
-      if (checksum("=" + data) == hash) 
-      {
-        // g-k declares an order position, G-K says you've started to go down the final stretch
-        // As far as I can tell, charAt is the only way to typecast back to char
-        debug("checksum confirmed: ="+data+hash,-100);
-        switch (data.charAt(0)) 
-        {
-          case 'g':
-            orderDeclared[0] = true;
-            break;
-          case 'h':
-            orderDeclared[1] = true;
-            break;
-          case 'i':
-            orderDeclared[2] = true;
-            break;
-          case 'j':
-            orderDeclared[3] = true;
-            break;
-          case 'k':
-            orderDeclared[4] = true;
-            break;
-          case 'G':
-            orderMoving[0] = true;
-            break;
-          case 'H':
-            orderMoving[1] = true;
-            break;
-          case 'I':
-            orderMoving[2] = true;
-            break;
-          case 'J':
-            orderMoving[3] = true;
-            break;
-          case 'K':
-            orderMoving[4] = true;
-            break;
-          case '?':
-            someoneDoesntKnow = true;
-            break;      
-        }
-        // if we were an unknown order but the rest have filled,  we can deduce ours
-        if (myOrder == -1) 
-        {
-          int numFalses = 0;
-          int newOrder = 0;
-          for (i=0;i<numBots;i++) 
-          {
-            if (orderDeclared[i] == false) 
-            
-            {
-              numFalses++;
-              newOrder = i;
-            }
-          }
-          newOrder += 1;
-          if (numFalses == 1) 
-          {
-            debug(">>New Order Deduced",-100);
-            foundOrder(newOrder);
-          } 
-        }
-      }      
+    //unknown value? send -1 and wait for 45. TODO: deduce position
+    if (pos == -1) {
+       delay(45000); //30 seconds before going
     }
-  }
-  
-  // If it's been more than 1.5 seconds, update our status
-  if ((millis() - statusTimer) > 1500L) 
-  {  
-    sendStatus();
-    statusTimer = millis();
-  }
-}
-
-// Let's us see the bot's logs on what numbers are taken
-void debugOrder() 
-{
-  int j;
-  String orderString = "Orders: ";
-  for (j=0;j<numBots;j++) 
-  {
-    orderString = orderString + String(orderDeclared[j]);
-  }
-  debug(orderString,-100);
-  String movingString = "Moving: ";
-  for (j=0;j<numBots;j++) 
-  {
-    movingString = movingString + String(orderMoving[j]);
-  }
-  debug(movingString,-100); 
-} 
-
-// Called automatically by communicate() to broadcast our status
-void sendStatus() 
-{
-  debugOrder();
-  
-  String data2 = "";
-  // If we have our order number and it's unique
-  if ((myOrder != 0)) 
-  {
-    // There's been an error and don't know my number
-    if (myOrder == -1) 
-    {
-      data2 = String("?");
-    } 
-    else if (imMoving) 
-    {
-      // Ascii G-K
-      data2 = String(char(myOrder + 70));
-    } 
-    else 
-    {
-      // Ascii g-k
-      data2 = String(char(myOrder + 102));
-    }
-    String hash = checksum("=" + data2);
-    String packet = "=" + data2 + hash;
-    debug("Sending: "+packet,-100);
-    Xbee.print(packet);
-  } 
-}
-
-
-// Returns 0 to wait, your (new) order number if it is time for you to go
-int doIGo() 
-{
-  debug(">>At black line, waiting to go.",-100);
-  timeSinceLastMoved = millis();
-  while(1) 
-  {
-    communicate();
-    sDelay(20);
     
-    // Timer that resets itself every time a new bot moves
-    if (arrayEqual(orderMoving,orderMovingLastChecked) == false) 
-    {
-      timeSinceLastMoved = millis();
-      // Fuck C. orderMovingLastChecked = orderMoving;
-      for (int j=0;j<numBots;j++) 
-      {
-        orderMovingLastChecked[j] = orderMoving[j];
-      }
-      debug(">>A bot has moved!",-100);
+    //time out
+    if (millis() > (15000 + pos*5000)) {
+        return;
     }
-    // Only set this the first time
-    if (grandFallbackTimer != 0L) 
-    {
-      grandFallbackTimer = millis();
-    }
-    // Everything is going right so far
-    if ((myOrder != -1) && (myOrder != 0)) 
-    {
-      // I am number one
-      if (myOrder == 1) 
-      {
-        debug(">>I'm going first",-100);
-        return myOrder;
-      }
-      // Other basic case: person in front of me has gone
-      if (orderMoving[myOrder-2] == true) 
-      {
-        debug(">>Bot in front has gone, I'm leaving as: ",myOrder);
-        return myOrder;
-      }
-      // They haven't gone yet, but the person ahead of them has 
-      // and it's been over 45 sec. We assume that either they went mute
-      // or the bot somehow totally died, so we go ahead anyways.
-      if ((myOrder > 2) && (orderMoving[myOrder-3] == true) && ((millis()-timeSinceLastMoved)>45000L)) 
-      {
-        debug(">>Timeout for bot ahead, I'm leaving as: ", myOrder);
-        return myOrder;
-      }
-      // Same thing, but I'm bot number two
-      if ((myOrder == 2) && (millis()-timeSinceLastMoved)>45000L) 
-      {
-        debug(">>Bot 1 has timed out and I'm bot two. Leaving as: ",myOrder);
-        return myOrder;
-      }
-    }
-    // Grand Fallback Time: in case everything has gone wrong, we can at least do a staggered start
-    long timeToWait = 0L;
-    if (myOrder == -1) 
-    {
-      timeToWait = 120000L;
-    } 
-    else if (myOrder == 0) 
-    {
-      timeToWait = 130000L;
-    } 
-    else 
-    {
-      timeToWait = 90000L + 10000L*myOrder;
-    }
-    if ((millis()-grandFallbackTimer) > timeToWait) 
-    {
-      if ((myOrder == -1) || (myOrder == 0)) 
-      {
-        myOrder = numBots; //Might as well just put them last
-      }
-      debug(">>Grand Fallback Time Exceeded. Leaving as: ", myOrder);
-      return myOrder;
+    
+    //grand timeout
+    if (millis() > 45000) {
+       return; 
     }
   }
 }
-
-// Call this when you know your number, or with -1 if you don't
-void foundOrder(int orderNum) 
-{
-  debug(">>Found Order, requesting number ",orderNum);
-  if ((orderNum <= 5) && (orderNum >= -1))
-  {
-    if ((orderDeclared[orderNum-1] == false) && (orderMoving[orderNum-1] == false) && (orderNum != -1)) 
-    {
-      myOrder = orderNum;
-      orderDeclared[myOrder-1] = true;
-    } 
-    else
-    {
-      debug(">>Order Number Conflict or unsuccessful. Now -1",-100);
-      myOrder = -1;
-      someoneDoesntKnow = true;
-    }
-  } 
-  else 
-  {
-    myOrder = -1;
-    someoneDoesntKnow = true;
-  }
-  debug(">>Found Order finished, we are now number  ",myOrder);
-  sendStatus();
-}
-
-// Yes, the length is hard coded. So don't use it in other code.
-boolean arrayEqual(boolean *a, boolean *b)
-{
-  int n;
-  int len = numBots;
-  for (n=0;n<len;n++) 
-  {
-    if (a[n]!=b[n]) 
-    {
-      return false;
-    }
-  }
-  return true;
-}
-
-// Sets state to let other bots know who has moved
-void sendMoving() 
-{
-  imMoving = true;
-  orderMoving[myOrder-1] = true;
-}
-
-// Little serial debugging function. -100 as int makes the int argument optional
-void debug(String text, int number) 
-{
- if (debugging) 
- {
-   String message = "";
-   if (number != -100) 
-   {
-     message = text + number;
-   } 
-   else 
-   {
-     message = text;
-   }
-   Serial.println(message);
- }
-} 
